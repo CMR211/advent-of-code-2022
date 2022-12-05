f83c54fea95841746c69b107eb87274c95918098
CMR211 <bartosz.surma211@gmail.com>
2022-12-05 11:13:36 +0100

    day-1

--- src/day-1/script.ts
+++ src/day-1/script.ts
@@ -10,9 +10,7 @@ function countCalories(elves: number[][]) {
 }
 
 function findBiggest(elves: number[]) {
-    const sorted: number[] = JSON.parse(JSON.stringify(elves)).sort((a: number, b: number) => b - a)
-    const biggestValue = sorted[0]
-    return elves.indexOf(biggestValue)
+    return JSON.parse(JSON.stringify(elves)).sort((a: number, b: number) => b - a)[0]
 }
 
 const part1answer = findBiggest(countCalories(parseInput(input)))
@@ -22,9 +20,10 @@ console.log(part1answer)
 
 function find3Biggest(elves: number[]) {
     const sorted: number[] = JSON.parse(JSON.stringify(elves)).sort((a: number, b: number) => b - a)
-    const biggest = sorted.slice(0, 3)
-    return biggest.reduce((a, b) => a + b, 0)
+    const biggest3 = sorted.slice(0, 3)
+    return biggest3.reduce((a, b) => a + b, 0)
 }
 
 const part2answer = find3Biggest(countCalories(parseInput(input)))
 console.log(part2answer)
+// 199628
