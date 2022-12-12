0a052dcb85b73c91c00cec13d9a024fdb5c4776e
cmr211 <bartosz.surma211@gmail.com>
2022-12-12 21:29:03 +0100

    update: day 6 solution

--- src/day-6/script.ts
+++ src/day-6/script.ts
@@ -2,15 +2,30 @@ import { raw } from "./raw"
 
 class Solution {
     get findBuffer() {
-        for (let index = 4; index < raw.length - 1; index++) {
-            const buffer = raw.slice(index-4,index)
-            for (let i = 0; i< 4;i++) {
-                const temp = buffer.slice()
-                temp[i] = "."
+        return this.detect(4)
+    }
+    get findMessage() {
+        return this.detect(14)
+    }
+    detect(length:number) {
+        for (let index = length; index < raw.length - 1; index++) {
+            const buffer = raw.slice(index - length, index)
+            let isUnique = true
+            for (let i = 0; i < length; i++) {
+                const char = buffer[i]
+                const regex = new RegExp(`${char}`, "g")
+                if (Array.from(buffer.matchAll(regex)).length > 1) isUnique = false
             }
+            if (!isUnique) continue
+            return index
         }
     }
 }
 
-const solution = new Solution
+const solution = new Solution()
+
 console.log(solution.findBuffer)
+// 1912
+
+console.log(solution.findMessage)
+
